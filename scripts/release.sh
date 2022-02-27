f52e3ad9e32c9b56fc80dd2ec13b35e55a8dfdbf
lhlyu <misakafs@gmail.com>
2022-02-27 14:55:07 +0800

    测试获取版本号

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,9 +4,10 @@
 BUYU_TAG=$(git describe --abbrev=0 --tags)
 BUYU_VERSION=$(echo $BUYU_TAG | grep -Eo "\d+.\d+.\d+")
 
-echo $BUYU_TAG, $BUYU_VERSION
-
-flutter pub get
-echo $KEYSTORE | base64 -di > android/app/keystore.jks
-flutter test
-flutter build apk --release  --target-platform android-arm
+echo $BUYU_TAG
+echo $BUYU_VERSION
+#
+#flutter pub get
+#echo $KEYSTORE | base64 -di > android/app/keystore.jks
+#flutter test
+#flutter build apk --release  --target-platform android-arm
