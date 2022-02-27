7e6b493a9a8559481bf11e21744c0c45d1b4ccd7
lhlyu <misakafs@gmail.com>
2022-02-27 20:46:41 +0800

    环境变量写入到$GITHUB_ENV

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,8 +8,8 @@ echo $GITHUB_TAG
 echo $BUYU_TAG
 echo $BUYU_VERSION
 
-export BUYU_TAG=$BUYU_TAG
-export BUYU_VERSION=$BUYU_VERSION
+echo "BUYU_TAG=$BUYU_TAG" >> $GITHUB_ENV
+echo "BUYU_VERSION=$BUYU_VERSION" >> $GITHUB_ENV
 
 flutter pub get
 echo $KEYSTORE | base64 -di > android/app/keystore.jks
