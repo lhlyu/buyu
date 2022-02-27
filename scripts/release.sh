b4b0aeced12ea40ac044cc1191fd463890d059ad
lhlyu <misakafs@gmail.com>
2022-02-27 20:23:02 +0800

    修改脚本

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 # 根据tag获取版本号
-BUYU_TAG=$(echo $GITHUB_TAG | grep -Eo "v\d+.\d+.\d+.*")
-BUYU_VERSION=$(echo $BUYU_TAG | grep -Eo "\d+.\d+.\d+")
+BUYU_TAG=$(echo $GITHUB_TAG | sed 's/.*\/v\(.*\).*/\1/g')
+BUYU_VERSION=$(echo $BUYU_TAG | sed 's/\(.*\)-.*/\1/g')
 
-echo 1:$GITHUB_TAG
-echo 2:$BUYU_TAG
-echo 3:$BUYU_VERSION
-echo 4:$GITHUB_TAG | cut -b 2,5
-echo 5:$GITHUB_TAG | sed 's/[0-9]//g'
+echo $GITHUB_TAG
+echo $BUYU_TAG
+echo $BUYU_VERSION
 
 #flutter pub get
 #echo $KEYSTORE | base64 -di > android/app/keystore.jks
