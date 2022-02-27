9b9cbc75a6905aa38471fb71624ad0e2d77ae05d
lhlyu <misakafs@gmail.com>
2022-02-27 15:36:00 +0800

    获取版本号

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # 根据tag获取版本号
 BUYU_TAG=$(echo $GITHUB_REF | grep -Eo "v\d+.\d+.\d+.*")
@@ -8,7 +8,7 @@ echo $GITHUB_REF
 echo $BUYU_TAG
 echo $BUYU_VERSION
 
-flutter pub get
-echo $KEYSTORE | base64 -di > android/app/keystore.jks
-flutter test
-flutter build apk --release  --target-platform android-arm
+#flutter pub get
+#echo $KEYSTORE | base64 -di > android/app/keystore.jks
+#flutter test
+#flutter build apk --release  --target-platform android-arm
