c3e11ee8b3cfe7afa4e3b2b1fa74184f47cdd6db
lhlyu <misakafs@gmail.com>
2022-02-27 15:42:14 +0800

    update ci

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # 根据tag获取版本号
-BUYU_TAG=$(echo $GITHUB_REF | grep -Eo "v\d+.\d+.\d+.*")
+BUYU_TAG=$(echo $GITHUB_TAG | grep -Eo "v\d+.\d+.\d+.*")
 BUYU_VERSION=$(echo $BUYU_TAG | grep -Eo "\d+.\d+.\d+")
 
-echo $GITHUB_REF
+echo $GITHUB_TAG
 echo $BUYU_TAG
 echo $BUYU_VERSION
 
