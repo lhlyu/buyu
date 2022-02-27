d6809107ee5051a4ac6277c9091c82cad0b5c3f4
lhlyu <misakafs@gmail.com>
2022-02-27 15:08:48 +0800

    update ci

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo '测试开始'
+
 # 根据tag获取版本号
 BUYU_TAG=$(git describe --abbrev=0 --tags)
 BUYU_VERSION=$(echo $BUYU_TAG | grep -Eo "\d+.\d+.\d+")
 
 echo $BUYU_TAG
 echo $BUYU_VERSION
+
+echo '测试结束'
 #
 #flutter pub get
 #echo $KEYSTORE | base64 -di > android/app/keystore.jks
