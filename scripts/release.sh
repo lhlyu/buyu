d838dde2ab85de6dace95d97f1a82b473103de7e
lhlyu <misakafs@gmail.com>
2022-02-27 15:20:09 +0800

    修改ci

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 根据tag获取版本号
-BUYU_TAG=${GITHUB_REF:10}
+BUYU_TAG=$(echo $GITHUB_REF | grep -Eo "v\d+.\d+.\d+.*")
 BUYU_VERSION=$(echo $BUYU_TAG | grep -Eo "\d+.\d+.\d+")
 
 echo $GITHUB_REF
 echo $BUYU_TAG
 echo $BUYU_VERSION
 
-flutter pub get
-echo $KEYSTORE | base64 -di > android/app/keystore.jks
-flutter test
-flutter build apk --release  --target-platform android-arm
+#flutter pub get
+#echo $KEYSTORE | base64 -di > android/app/keystore.jks
+#flutter test
+#flutter build apk --release  --target-platform android-arm
